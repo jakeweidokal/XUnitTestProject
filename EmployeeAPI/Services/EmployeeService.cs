446caccb566a9019a125886cf86b6902babe0096
Jacob Weidokal <jacob.t.weidokal@nasa.gov>
2022-11-15 14:20:21 -0500

    implement IEmployeeService interface

--- EmployeeAPI/Services/EmployeeService.cs
+++ EmployeeAPI/Services/EmployeeService.cs
@@ -1,6 +1,38 @@
-﻿namespace EmployeeAPI.Services
+﻿using EmployeeAPI.Interfaces;
+using EmployeeAPI.Models;
+
+namespace EmployeeAPI.Services
 {
-    public class EmployeeService
+    public class EmployeeService : IEmployeeService
     {
+        public List<Employee> Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> Update(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
